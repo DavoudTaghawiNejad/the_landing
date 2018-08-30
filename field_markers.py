e7eccefc2f7b58ff5fd06ae0867734f3bb5c9070
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-08-30 10:12:56 -0300

    Field markers for premarked fields

--- field_markers.py
+++ field_markers.py
@@ -3,7 +3,7 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 from reportlab.pdfgen import canvas
-from reportlab.lib.units import mm, inch
+from reportlab.lib.units import mm
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import Paragraph, Frame
 from reportlab.lib.styles import getSampleStyleSheet
@@ -20,9 +20,7 @@ def main():
     c = canvas.Canvas("field_markers.pdf")
 
     p = (0, 0)
-    for letter in range(ord('A'), ord('K')):
-        p = writepdf(1, False, chr(letter), p, c)
-    for letter in range(ord('A'), ord('K')):
+    for letter in range(ord('A'), ord('X')):
         p = writepdf(1, False, chr(letter), p, c)
 
 
@@ -36,19 +34,22 @@ def writepdf(number, effort, letter, p, canvas):
     styleN.spaceAfter = 10
     styleH = styles['Heading1']
     styleH.alignment = 1
+    styleH.spaceBefore = 26
     styleN.alignment = 1
     story = []
     story.append(Paragraph('<font color="black">%s</font>' % letter, styleH))
+    story.append(Paragraph(' ', styleH))
+    story.append(Paragraph('<font color="black">%s</font>' % letter, styleH))
 
-    canvas.saveState()
-    f = Frame(spacer + p[0] * inch * 0.5, spacer + p[1] * inch + 0.3 * inch, inch  * 0.5, inch, showBoundary=1)
+    f = Frame(spacer + p[0] * 20 * mm, spacer + p[1] * 20 * mm * 2 + 0.3 * 20 * mm, 20 * mm, 40 * mm, showBoundary=1)
     f.addFromList(story, canvas)
-    canvas.restoreState()
-    if p == (6, 9):
+
+
+    if p == (4, 4):
         canvas.showPage()
         p = (0, 0)
     else:
-        if p[1] == 9:
+        if p[1] == 4:
             p = (p[0] + 1, 0)
         else:
             p = (p[0], p[1] + 1)
