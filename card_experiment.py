2d0eb5eadff4b7ab6974737bfd20ce4f59cc211d
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-09-03 13:41:42 -0300

    Make initial going to 2x2 most important

--- card_experiment.py
+++ card_experiment.py
@@ -169,7 +169,7 @@ def one_game(directions=None, figs=False):
                 penelty += 0.1
             movement.append(m)
             if subround <= 3 * 1:
-                penelty += 2 * sum([(2 - p.x) ** 2 + (2 - p.y) ** 2 for p in pos])
+                penelty += 2 * sum([(2 - p.x) ** 2 + (2 - p.y) ** 2 for p in pos]) * 2
             else:
                 penelty += 2 * sum([(3 - p.x) ** 2 + (3 - p.y) ** 2 for p in pos])
             if drawn == Cards.TRIBE_EVENT and drawn.tribe_affected <= tribes:
